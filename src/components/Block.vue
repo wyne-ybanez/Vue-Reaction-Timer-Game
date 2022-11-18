e3755ad9ca566112afb3b9b5272071431e22cd36
wyne-ybanez <wyne_01@icloud.com>
2022-11-18 16:40:07 +0000

    Update: Add lifecycle hooks

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,39 @@
 <template>
-  <div class="block">click me!</div>
+  <!-- This block will show after the delay -->
+  <div class="block" v-if="showBlock">click me!</div>
 </template>
 
 <script>
 export default {
   props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+    }
+  },
+  /*
+   Used to run code after the component has finished the initial rendering and created the DOM nodes
+   Mounting which sets a delay timer - the delay prop is the variable
+  */
+  mounted() {
+    console.log('component mounted')
+    setTimeout(() => {
+      this.showBlock = true
+      console.log(this.delay)
+    }, this.delay)
+  },
+  /*
+   Fires this after the component has updated
+  */
+  updated() {
+    console.log('component updated')
+  },
+  /*
+   Fires this when the component unmounts - especially when changing pages
+  */
+  unmounted() {
+    console.log('component unmounted')
+  }
 }
 </script>
 
